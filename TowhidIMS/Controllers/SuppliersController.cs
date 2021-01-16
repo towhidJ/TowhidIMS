8794d831a21ef7c9d1d669ce8d8824198996dea1
6t4 <towhidulislamcse@gmail.com>
2021-01-16 11:24:51 +0600

    Supplier Update

---
+++ TowhidIMS/Controllers/SuppliersController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TowhidIMS.Models.Codefirst;
+
+namespace TowhidIMS.Controllers
+{
+    public class SuppliersController : Controller
+    {
+        private dbcontext db = new dbcontext();
+
+        // GET: Suppliers
+        public ActionResult Index()
+        {
+            return View(db.Suppliers.ToList());
+        }
+
+        // GET: Suppliers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Supplier supplier = db.Suppliers.Find(id);
+            if (supplier == null)
+            {
+                return HttpNotFound();
+            }
+            return View(supplier);
+        }
+
+        // GET: Suppliers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Address,Balance,CreateDate,UpdateDate,bizId")] Supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                supplier.bizId = SupplierBizId();
+                supplier.CreateDate = DateTime.Today.Date;
+                db.Suppliers.Add(supplier);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(supplier);
+        }
+
+        // GET: Suppliers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Supplier supplier = db.Suppliers.Find(id);
+            if (supplier == null)
+            {
+                return HttpNotFound();
+            }
+            return View(supplier);
+        }
+
+        // POST: Suppliers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Address,Balance,CreateDate,UpdateDate,bizId")] Supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(supplier).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(supplier);
+        }
+
+        // GET: Suppliers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Supplier supplier = db.Suppliers.Find(id);
+            if (supplier == null)
+            {
+                return HttpNotFound();
+            }
+            return View(supplier);
+        }
+
+        // POST: Suppliers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Supplier supplier = db.Suppliers.Find(id);
+            db.Suppliers.Remove(supplier);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        public string SupplierBizId()
+        {
+            string depName = "SKF";
+            string month = DateTime.Now.ToString("yyyyMM");
+
+            int count = db.Suppliers.Select(c => c).Count();
+
+            string countString = count.ToString();
+            if (countString.Length == 1)
+            {
+                string newNumber = "0" + countString;
+                string appNumber = depName + "-" + month + newNumber;
+                return appNumber;
+            }
+            else
+            {
+                string newNumber = countString;
+                string appNumber = depName + "-" + month + newNumber;
+                return appNumber;
+            }
+
+        }
+    }
+}
